56bb3bd1a103edddf4ac4812c87f59c6318d20d7
Juan Pablo Tabares <juancho9615@hotmail.com>
2023-03-31 19:08:09 -0500

    Moviendo .css import

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import './index.css'
 
 import { App } from './App'
 
