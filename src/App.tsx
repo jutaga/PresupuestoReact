00d764ed1fc69dea3837ce211e079374f7d42675
Juan Pablo Tabares <juancho9615@hotmail.com>
2023-03-31 19:03:39 -0500

    Agregando Carga condicional statesTotal

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,36 @@
+import { useState } from "react"
 import { Pregunta } from "./components/Pregunta"
+import { Formulario } from "./components/Formulario";
 
 export const App = () => {
+
+  const [presupuesto, setPresupuesto] = useState<number>(0);
+  const [total, setTotal] = useState<number>(0);
+  const [mostrarPregunta, setMostrarPregunta] = useState<boolean>(true);
+
+
   return (
     <div className="container">
-      <header>
+      <header >
         <h1 className="m-5 text-center fs-1 fw-bolder">
           <i className="bi bi-bag-fill"></i> Gasto Semanal</h1>
 
-        <div className="bg-light p-5 border-dark rounded-3">
-          <Pregunta />
+        <div className="bg-light p-5 border-dark rounded-3" >
+          {
+            mostrarPregunta
+              ?
+              <Pregunta setPresupuesto={setPresupuesto} setTotal={setTotal} setMostrarPregunta={setMostrarPregunta} />
+              :
+              <div className="row">
+                <div className="col-md-6">
+                  <Formulario />
+                </div>
+                <div className="col-md-6">
+                  2
+                </div>
+              </div>
+          }
         </div>
-
       </header>
     </div>
   )
