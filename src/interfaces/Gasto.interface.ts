22b567cdbe6bf34baad1edf999a11e29b1d70f2e
Juan Pablo Tabares <juancho9615@hotmail.com>
2023-04-01 15:08:51 -0500

    agregando interfaces

---
+++ src/interfaces/Gasto.interface.ts
@@ -0,0 +1,5 @@
+export interface Gasto {
+    nombre: string;
+    cantidad: number;
+    id: string;
+  }
---
+++ src/interfaces/Props.interface.ts
@@ -0,0 +1,5 @@
+export interface PreguntaProps {
+    setPresupuesto: React.Dispatch<React.SetStateAction<number>>;
+    setTotal: React.Dispatch<React.SetStateAction<number>>;
+    setMostrarPregunta: React.Dispatch<React.SetStateAction<boolean>>
+}
